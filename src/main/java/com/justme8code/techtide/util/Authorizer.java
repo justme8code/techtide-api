b75309577fb13146a238a4a660dba1649a81e6a4
justme8code <jvmMonster1@gmail.com>
2025-02-13 10:41:50 +0100

    Removed unused Java files: Authorizer.java and ErrorResponse.java

--- src/main/java/com/justme8code/techtide/util/Authorizer.java
+++
@@ -1,8 +0,0 @@
-package com.justme8code.techtide.util;
-
-import io.jsonwebtoken.Claims;
-
-public interface Authorizer {
-    Claims validateToken(String token);
-    String generateToken(String userId);
-}
